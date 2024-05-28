c018f876d1bf83f5631ca762d648b0ff61eff365
“Rorianav” <vergararoriana@gmail.com>
2024-05-27 21:40:16 -0400

    Initial Commit

---
+++ docWall/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  docWall
+//
+//  Created by Roriana Vergara on 5/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ docWall/docWallApp.swift
@@ -0,0 +1,17 @@
+//
+//  docWallApp.swift
+//  docWall
+//
+//  Created by Roriana Vergara on 5/27/24.
+//
+
+import SwiftUI
+
+@main
+struct docWallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
